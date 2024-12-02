3bf9377cb20734897e4a83977b69b7bd8d5c689e
Yunfan Yang <cloudy.young@outlook.com>
2024-12-02 03:03:47 -0700

    Fix json types again

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -141,14 +141,11 @@ const getIndexContent = (models: DMMF.Model[]) => {
 
 const getUtilsContent = () => {
   return `
-    import { z } from 'zod'
-    import { Prisma } from '@prisma/client'
+    import { z } from "zod"
 
-    export const LiteralSchema = z.union([z.string(), z.number(), z.boolean()])
+    export const LiteralSchema = z.union([z.string(), z.number(), z.boolean(), z.null()])
     export type Literal = z.infer<typeof LiteralSchema>
-    export const JsonSchema: z.ZodType<Prisma.NullableJsonNullValueInput | Prisma.InputJsonValue> = z.lazy(() =>
-      z.union([LiteralSchema, z.array(JsonSchema), z.record(JsonSchema)]),
-    )
+    export const JsonSchema: z.ZodType = z.lazy(() => z.union([LiteralSchema, z.array(JsonSchema), z.record(JsonSchema)]))
     export type Json = z.infer<typeof JsonSchema>
   `
 }
--- packages/usage/types/utils.ts
+++ packages/usage/types/utils.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod'
-import { Prisma } from '@prisma/client'
 
-export const LiteralSchema = z.union([z.string(), z.number(), z.boolean()])
+export const LiteralSchema = z.union([
+  z.string(),
+  z.number(),
+  z.boolean(),
+  z.null(),
+])
 export type Literal = z.infer<typeof LiteralSchema>
-export const JsonSchema: z.ZodType<
-  Prisma.NullableJsonNullValueInput | Prisma.InputJsonValue
-> = z.lazy(() =>
+export const JsonSchema: z.ZodType = z.lazy(() =>
   z.union([LiteralSchema, z.array(JsonSchema), z.record(JsonSchema)]),
 )
 export type Json = z.infer<typeof JsonSchema>
