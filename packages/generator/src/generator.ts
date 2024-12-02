a6938c4030d9f953c7da95ff8b03175e18852d97
Yunfan Yang <cloudy.young@outlook.com>
2024-12-02 02:30:39 -0700

    Fix JSON type

--- packages/generator/src/generator.ts
+++ packages/generator/src/generator.ts
@@ -137,15 +137,14 @@ const getIndexContent = (models: DMMF.Model[]) => {
 
 const getUtilsContent = () => {
   return `
-    import { z } from 'zod';
+    import { z } from 'zod'
+    import { Prisma } from '@prisma/client'
 
-    export const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()])
-    export type Literal = z.infer<typeof literalSchema>
-    export type Json = Literal | { [key: string]: Json } | Json[]
-    export const JsonSchema: z.ZodType<Json> = z.lazy(() =>
-      z.union([literalSchema, z.array(JsonSchema), z.record(JsonSchema)]),
-    ).transform((val) => {
-      return val ? JSON.parse(JSON.stringify(val)) : val
-    })
+    export const LiteralSchema = z.union([z.string(), z.number(), z.boolean()])
+    export type Literal = z.infer<typeof LiteralSchema>
+    export const JsonSchema: z.ZodType<Prisma.InputJsonValue> = z.lazy(() =>
+      z.union([LiteralSchema, z.array(JsonSchema), z.record(JsonSchema)]),
+    )
+    export type Json = z.infer<typeof JsonSchema>
   `
 }
--- packages/usage/types/utils.ts
+++ packages/usage/types/utils.ts
@@ -1,13 +1,9 @@
 import { z } from 'zod'
+import { Prisma } from '@prisma/client'
 
-export const literalSchema = z.union([
-  z.string(),
-  z.number(),
-  z.boolean(),
-  z.null(),
-])
-export type Literal = z.infer<typeof literalSchema>
-export type Json = Literal | { [key: string]: Json } | Json[]
-export const JsonSchema: z.ZodType<Json> = z.lazy(() =>
-  z.union([literalSchema, z.array(JsonSchema), z.record(JsonSchema)]),
+export const LiteralSchema = z.union([z.string(), z.number(), z.boolean()])
+export type Literal = z.infer<typeof LiteralSchema>
+export const JsonSchema: z.ZodType<Prisma.InputJsonValue> = z.lazy(() =>
+  z.union([LiteralSchema, z.array(JsonSchema), z.record(JsonSchema)]),
 )
+export type Json = z.infer<typeof JsonSchema>
